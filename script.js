a7681a06bd6510847e030e87a2a11ddb268a8311
Charles32-Dev <Charles_fanautico@hotmail.com>
2025-02-11 12:28:48 -0300

    adicionando novo projeto jokenpo

---
+++ script.js
@@ -0,0 +1,50 @@
+const result = document.querySelector('.result')
+const HumanScore = document.querySelector('#your-score')
+const MachineScore = document.querySelector('#machine-score')
+
+let ScoreHuman = 0
+let ScoreMachine = 0
+
+const PlayerHuman = (Humanchoice) => {
+
+    PlayerTheGame(Humanchoice, PlayerMachine())
+}
+
+
+const PlayerMachine = () => {
+    const Choice = ['rock', 'paper', 'scissors']
+
+    const NumberRandon = Math.floor(Math.random() * 3)
+
+    return Choice[NumberRandon]
+}
+
+
+const PlayerTheGame = (human, machine) => {
+    console.log(' Human: ' + human + ' Machine: ' + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Empatou!"
+        result.style.color = "#BC3E3E"
+    }
+
+    else if (
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')
+    ) {
+        ScoreHuman++
+        HumanScore.innerHTML = ScoreHuman
+        result.style.color = "#87E906"
+        result.innerHTML = "Você ganhou, Parabéns!"
+
+
+    } else {
+        ScoreMachine++
+        MachineScore.innerHTML = ScoreMachine
+        result.style.color = "#BC3E3E"
+        result.innerHTML = "Você perdeu para a Alexa"
+    }
+}
+
+
